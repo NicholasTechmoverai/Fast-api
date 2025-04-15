5860b058f1fef81f9eebca28b7f758c4f0c326c9
NicholasTechmoverai <techmoversai@gmail.com>
2025-04-15 18:44:33 +0300

    /.//

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class Config:
         "http://192.168.100.2:5000",
         "http://localhost:5000",
         "http://127.0.0.1:5000",
-        "http://localhost:8080",
+        "http://localhost:8080"
         "http://192.168.100.2:8080"
     ]
 
